e4efb57476c66c854e731d8540ce79c71f44fd62
ragib <ragibn5@gmail.com>
2024-04-10 17:17:53 +0600

    Removed dummy tests and finalized setup.

--- test/dart_extended_exceptions_test.dart
+++ test/dart_extended_exceptions_test.dart
@@ -1,8 +1,7 @@
-import 'package:dart_extended_exceptions/dart_extended_exceptions.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Collective-Test', () {
-    throw IllegalArgumentException("Illegal argument exception");
+    // todo
   });
 }
